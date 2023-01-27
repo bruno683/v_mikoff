2cfa39b4a7c4fa562437eb5c714fbafe5f5dbd02
Bruno Richard <bruno683@outlook.fr>
2023-01-27 17:25:47 +0100

    Form contact added to home page and install ORM fixtures

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,23 +3,58 @@
 namespace App\Controller;
 
 use App\Entity\Article;
+use App\Form\ContactType;
+use Symfony\Component\Mime\Email;
 use App\Repository\ArticleRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(ArticleRepository $articleRepo ): Response
+    public function index(ArticleRepository $articleRepo, Request $request, MailerInterface $mailer ): Response
     {
         $title = "Vivianne Mikoff";
         $article = new Article();
         $posts = $articleRepo->findPostPublished($article);
+
+        $form = $this->createForm(ContactType::class);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+            $adress = $data['Email'];
+            $name = $data['LastName'];
+            $subject = $data['subject'];
+            $firstName= $data['firstName'];
+            $message = $data['message'];
+            //header('Access-Control-Allow-Origin: *');
+            
+            $email = (new Email())
+            ->from($name ." ". $firstName . " " . "<" . $adress .">")
+            ->to('Bruno683@outlook.fr')
+            ->replyTo($adress)
+            ->subject($subject)
+            ->text($message);
+            
+
+            $mailer->send($email);
+
+            $this->addFlash('success', 'Votre message à été envoyé avec succès !');
+
+            return $this->redirectToRoute('app_contact');
+        }
+
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
             'title' => $title,
             'posts' =>  $posts,
+            'form' => $form->createView()
         ]);
     }
 }
