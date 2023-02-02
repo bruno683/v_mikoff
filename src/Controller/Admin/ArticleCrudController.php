6751a837e8d3aa75ae42bedc2bf91c8026defe94
Bruno Richard <bruno683@outlook.fr>
2023-02-02 12:56:52 +0100

    article Crud added

--- src/Controller/Admin/ArticleCrudController.php
+++ src/Controller/Admin/ArticleCrudController.php
@@ -3,7 +3,13 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Article;
+use Doctrine\ORM\EntityManagerInterface;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 
 class ArticleCrudController extends AbstractCrudController
 {
@@ -12,14 +18,22 @@ class ArticleCrudController extends AbstractCrudController
         return Article::class;
     }
 
-    /*
+    
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
+            IdField::new('id')->hideOnIndex()->hideOnForm(),
             TextField::new('title'),
-            TextEditorField::new('description'),
+            TextEditorField::new('content'),
+            BooleanField::new('isPublished')->onlyOnForms(),
+            DateTimeField::new('createdAt'),
+            DateTimeField::new('updatedAt')->hideOnIndex(),
         ];
     }
-    */
+
+    public function persistEntity(EntityManagerInterface $entityManager, $entityInstance): void
+    {
+        dd($entityInstance);
+    }
+    
 }
--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -3,8 +3,10 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Article;
+use PhpParser\Node\Expr\New_;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
@@ -30,7 +32,7 @@ class DashboardController extends AbstractDashboardController
             ->generateUrl();
 
         
-        return $this->redirect('$url');
+        return $this->redirect($url);
 
     }
 
@@ -43,6 +45,10 @@ class DashboardController extends AbstractDashboardController
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+        yield MenuItem::section('Articles');
+        yield MenuItem::subMenu('actions', 'fas fa-list')->setSubItems([
+            MenuItem::linkToCrud('Listes des articles', 'fas fa-eye', Article::class),
+            MenuItem::linkToCrud('Ajouter un article', 'fas fa-plus', Article::class)->setAction(Crud::PAGE_NEW),
+        ]);
     }
 }
