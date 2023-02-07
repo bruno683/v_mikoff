82263c615a2964ec56fbce39fbee9ab3f0d5fcac
Bruno Richard <bruno683@outlook.fr>
2023-02-07 17:08:17 +0100

    Adding some animations to intro, card and form class

--- assets/app.js
+++ assets/app.js
@@ -6,6 +6,46 @@
  */
 // app.js
 
+const cards = document.querySelectorAll('.card');
+const form = document.querySelector('.section-form');
+const intro = document.querySelector('.intro');
+
+function checkCard() {
+    const triggerBottom = window.innerHeight /5 *6;
+
+    //transform all cards
+    cards.forEach(card => {
+        const cardTop = card.getBoundingClientRect().top;
+
+        if(cardTop < triggerBottom) {
+            card.classList.add('show')
+        }else {
+            card.classList.remove('show')
+        }
+    })
+    // tranform form
+    const formTop = form.getBoundingClientRect().top
+
+    if (formTop < triggerBottom) {
+        form.classList.add('show')
+    }else {
+        form.classList.remove('show')
+    }
+    //transfom intro class object
+    const introTop = intro.getBoundingClientRect().top;
+
+    if (introTop < triggerBottom) {
+        intro.classList.add('show')
+    }else {
+        intro.classList.remove('show')
+    }
+}
+
+
+window.addEventListener('scroll', checkCard);
+
+
+
 const $ = require('jquery');
 // this "modifies" the jquery module: adding behavior to it
 // the bootstrap module doesn't export/return anything
@@ -20,6 +60,8 @@ require('bootstrap');
 $(document).ready(function() {
     $('[data-toggle="popover"]').popover();
 });
+
+
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.scss';
 
